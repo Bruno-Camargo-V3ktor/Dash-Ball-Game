64992052e5a5db51bdf0bbecdbd899d458735648
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-17 18:12:50 -0300

    feat: create system by player movement

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_player).chain())
+        .add_systems(Update, player_movement)
         .run();
 }
+
+pub const PLAYER_SPEED: f32 = 500.0;
 
 #[derive(Component)]
 pub struct Player {}
@@ -34,4 +37,36 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         Camera2d::default(),
         Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
     ));
+}
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.single_mut() {
+        let mut dir = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+            dir += Vec3::new(-1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
+            dir += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+            dir += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+            dir += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if dir.length() > 0.0 {
+            dir = dir.normalize();
+        }
+
+        transform.translation += dir * (PLAYER_SPEED * time.delta_secs());
+    }
 }
