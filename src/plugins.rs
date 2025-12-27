33d884ff460cfc937d940fefcda3184eee8df1b8
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-27 12:07:37 -0300

    feat: create PlayerPlugin and CameraPlugin for initilize System and resources

--- src/plugins.rs
+++ src/plugins.rs
@@ -1,14 +1,17 @@
 use crate::{
     resources::timers::EnemySpawnTimer,
-    systems::enemy::{
-        confine_enemy, enemy_hit_player, enemy_movement, spawn_enemies, spawn_enemys_over_time,
-        update_enemy_direction,
+    systems::{
+        camera::{camera_position, spawn_camera},
+        enemy::{
+            confine_enemy, enemy_hit_player, enemy_movement, spawn_enemies, spawn_enemys_over_time,
+            update_enemy_direction,
+        },
+        player::{confine_player, player_hit_star, player_movement, spawn_player},
     },
 };
 use bevy::prelude::*;
 
 pub struct EnemyPlugin;
-
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<EnemySpawnTimer>()
@@ -24,5 +27,23 @@ impl Plugin for EnemyPlugin {
                 )
                     .chain(),
             );
+    }
+}
+
+pub struct PlayerPlugin;
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player).add_systems(
+            Update,
+            (player_movement, player_hit_star, confine_player).chain(),
+        );
+    }
+}
+
+pub struct CameraPlugin;
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, camera_position);
     }
 }
